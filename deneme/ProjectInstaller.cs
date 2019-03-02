f3132b3695e0f479b48c5cc9c6d41bb14776f211
Suleyman.Kilic <Suleyman.Kilic@DT379>
2019-03-02 20:05:33 +0300

    ok

---
+++ deneme/ProjectInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace deneme
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.serviceInstaller1.ServiceName = "suleyman";
+        }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
--- deneme/Service1.cs
+++ deneme/Service1.cs
@@ -6,23 +6,93 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using System.Threading;
+using Microsoft.Web.Administration;
+
 
 namespace deneme
 {
+
+
     public partial class Service1 : ServiceBase
     {
+        public static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public Service1()
         {
             InitializeComponent();
         }
+        private System.Threading.Timer myTimer;
+
 
         protected override void OnStart(string[] args)
         {
+            log.Info("servis basladi");
+            startTimer();
         }
 
+        private void startTimer()
+        {
+            try
+            {
+                if (myTimer == null)
+                {
+               
+                    int timerIntervalSecs = 60*60*8;
+                    TimeSpan tsInterval = new TimeSpan(0, 0, timerIntervalSecs);
+                    myTimer = new System.Threading.Timer(
+                        new System.Threading.TimerCallback(Timer_Tick)
+                        , null, tsInterval, tsInterval);
+                }
+            }
+            catch (Exception ex)
+            {
+                stopTimer();
+                log.Info("servis basladi");
+            }
+        }
+        private void stopTimer()
+        {
+            try
+            {
+                if (myTimer != null)
+                {
+              
+                    myTimer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+                    myTimer.Dispose();
+                    myTimer = null;
+                }
+            }
+            catch (Exception )
+            {
+                log.Info("servis basladi");
+            }
+        }
+
+        private void Timer_Tick(object state)
+        {
+            try
+            {
+
+
+                var serverManager = new ServerManager();
+                foreach (var site in serverManager.Sites)
+                {
+                    string sitePath = (site.LogFile.Directory + "WSVC" + site.Id);
+                }
+            }
+            catch (Exception )
+            {
+                log.Info("servis basladi");
+            }
+        }
+
+ 
         protected override void OnStop()
         {
+            stopTimer();
+            Log("servis durdu");
         }
     }
 }
